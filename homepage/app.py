8491a217d3c7dc0a7498253af79907955f489bb2
minhye bak <minhye4825@naver.com>
2023-02-05 14:57:38 +0800

    3

--- homepage/app.py
+++ homepage/app.py
@@ -1,14 +1,76 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import sqlite3
+import random
 
-#Flask 객체 인스턴스 생성
 app = Flask(__name__)
 
-@app.route('/') # 접속하는 url
-def index():
-  return render_template('index.html')
+loaded_food_names = []
+loaded_tags = set()
+donteat = "donteat"
 
-if __name__=="__main__":
-  app.run(host='localhost', port=8000)
-  # host 등을 직접 지정하고 싶다면
-  # app.run(host="127.0.0.1", port="5000", debug=True)
+def get_random_food_and_tag():
+    conn = sqlite3.connect('amuguna.db')
+    c = conn.cursor()
+    food_name = None
+    tags = None
+    c.execute("SELECT COUNT(*) FROM amuguna_food WHERE food_name NOT IN ({0})".format(','.join(['?']*len(loaded_food_names))), loaded_food_names)
+    count = c.fetchone()[0]
+    if count == 0:
+        conn.close()
+        return None, None
+    while food_name is None or any(tag in loaded_tags for tag in tags):
+        c.execute("SELECT food_name, tag1, tag2, tag3, tag4 FROM amuguna_food WHERE food_name NOT IN ({0}) ORDER BY RANDOM() LIMIT 1".format(','.join(['?']*len(loaded_food_names))), loaded_food_names)
+        food = c.fetchone()
+        if food is None:
+            break
+        food_name = food[0]
+        tags = (food[1], food[2], food[3], food[4])
+    loaded_tags.update(tags)
+    conn.close()
+    return food_name, tags
+
+
+@app.route('/')
+def index():
+    food_name, tags = get_random_food_and_tag()
+    if food_name:
+        random_tag = random.choice(tags)
+        return render_template('index.html', food_name=food_name, tags=tags, random_tag=random_tag)
+    else:
+        return render_template('index.html', donteat='Don\'t Eat')
+
+@app.route('/select', methods=['POST'])
+def select():
+    loaded_food_names.clear()
+    loaded_tags.clear()
+    return redirect(url_for('congrats'))
+
+@app.route('/congrats')
+def congrats():
+    return render_template('chuka.html')
+
+@app.route('/tag', methods=['POST'])
+def tag():
+    clicked_tag = request.form['tag']
+    loaded_tags.add(clicked_tag)
+    new_food_name, new_tags = get_random_food_and_tag()
+    while any(tag in new_food_name for tag in loaded_tags):
+        new_food_name, new_tags = get_random_food_and_tag()
+    random_tag = random.choice(new_tags)
+    return render_template('index.html', food_name=new_food_name, tags=new_tags, random_tag=random_tag)
+
+@app.route('/gunang', methods=['POST'])
+def gunang():
+    if 'random_tag' in request.form:
+        loaded_tags.remove(request.form['random_tag'])
+    else:
+        # Handle the case where the key 'random_tag' does not exist
+        pass
+
+    new_food_name, new_tags = get_random_food_and_tag()
+    random_tag = random.choice(new_tags)
+    return render_template('index.html', food_name=new_food_name, tags=new_tags, random_tag=random_tag)
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000)
